d6ecc389fa814995f832faf6c6b6f4d8ff6d04a1
Lavya Yalamanchi <ly503@nyu.edu>
2015-04-27 19:08:52 -0400

    Create madlib.php

---
+++ madlib.php
@@ -0,0 +1,14 @@
+<?php
+
+$noun = $_POST['noun'];
+$adj = $_POST['adj'];
+$verb = $_POST['verb'];
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$noun2 = $_POST['noun2'];
+$adj2 = $_POST['adj2'];
+$verb2 = $_POST['verb2'];
+
+echo "The " . " " . $num1 . " " .  $noun . " " . $verb . " the " .  $adj . " " . $noun2 . " . The two were so mad that they " . $verb2 . " " . $num2 . " times around the " . $adj2 . " house."
+
+?>
