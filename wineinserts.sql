7961d520ca7e607b3c78e9a94e69c806c1c41258
Lavya Yalamanchi <ly503@nyu.edu>
2015-04-27 19:15:02 -0400

    Create wineinserts.sql

---
+++ wineinserts.sql
@@ -0,0 +1,55 @@
+INSERT INTO Wine VALUES (NULL, "Castillo de Jumilla", 2010, "Spain", "Red", "Mourvedre", "ripe black fruit", "Castillo de Jumilla", 1, 1);
+INSERT INTO Wine VALUES (NULL, "Ique", 2013, "Argentina", "Red","Malbec", "pepper aroma", "Enrique Foster", 2, 2);
+INSERT INTO Wine VALUES (NULL, "Periquita", 2011, "Portugal", "Red", "Blend", "raspberry", "J.M. Fonseca", 3, 3);
+INSERT INTO Wine VALUES (NULL, "Pink Pegau", 2012, "France", "Rose", NULL, "peach and redcurrant", "Domaine du Pegau", 4, 4);
+INSERT INTO Wine VALUES (NULL, "Josef Leitz - Dragonstone", 2012, "Germany", "White", "Riesling", "honeysuckle", "Josef Leitz", 5, 5);
+INSERT INTO Wine VALUES (NULL, "Hawk Crest", 2008, "U.S.A.", "White", "Chardonnay",  NULL, "Hawk Crest", 6, 6);
+INSERT INTO Wine VALUES (NULL, "Babich", 2013, "New Zealand", "White", "Sauvignon Blanc", "fennel and citrus", "Babich", 7, 7);
+INSERT INTO Wine VALUES (NULL, "Verduzzo Friuli - Ronchi di Cialla",2005, "Italy", "White", "Blend", "apples and pears", "Ronchi di Cialla", 8, 8);
+INSERT INTO Wine VALUES (NULL, "Fleur du Cap", 2013, "South Africa",  "White", "butterscotch and nut", "Chardonnay", "Fleur du Cap", 9, 9);
+INSERT INTO Wine VALUES (NULL, "Le Dix de Los Vascos",2011, "Chile", "Red", "Cabernet Sauvignon", "licorice and spice", "Los Vascos", 10, 10);
+
+
+INSERT INTO Store VALUES (NULL, "Wines and Dimes", "9172345678", "5", "Bleecker Street", "New York City", "10012", "Greenwich Village", "WinesAndDimes@gmail.com", "$", "http://i6.cims.nyu.edu/~ly503/CSCI-UA.0060-1-Coursework/WinesandDimes.html");
+INSERT INTO Store VALUES (NULL, "Which One Wines?", "9171317890", "67", "East 5th Street", "New York City", "10009", "East Village", "WhichOneWines@gmail.com", "$", "http://i6.cims.nyu.edu/~ly503/CSCI-UA.0060-1-Coursework/Whichonewines.html");
+INSERT INTO Store VALUES (NULL, "Wines R Us", "9171234545", "122", "Mulberry Street", "New York City", "10002", "Lower East Side", "WinesRUs@yahoo.com", "$$", "http://i6.cims.nyu.edu/~ly503/CSCI-UA.0060-1-Coursework/WinesRUs.html");
+INSERT INTO Store VALUES (NULL, "Neighborhood Wines", "9178901234", "255", "Greenwich Street", "New York City", "10014", "Hudson Square", "neighborhoodwines@yahoo.com", "$$", "http://i6.cims.nyu.edu/~ly503/CSCI-UA.0060-1-Coursework/NeighborhoodWines.html");
+INSERT INTO Store VALUES (NULL, "Drunken Sunday Wines", "9171356789", "1", "Bushwick Lane", "New York City", "11211", "Williamsburg", "drunkensunday@gmail.com", "$$$", "http://i6.cims.nyu.edu/~ly503/CSCI-UA.0060-1-Coursework/drunkensunday.html");
+
+
+INSERT INTO StoreSells VALUES (NULL, 1, 1, 7.95);
+INSERT INTO StoreSells VALUES (NULL, 1, 2, 8.95);
+INSERT INTO StoreSells VALUES (NULL, 2, 3, 8.95);
+INSERT INTO StoreSells VALUES (NULL, 3, 4, 21.95);
+INSERT INTO StoreSells VALUES (NULL, 3, 5, 16.95);
+INSERT INTO StoreSells VALUES (NULL, 4, 6, 11.95);
+INSERT INTO StoreSells VALUES (NULL, 4, 7, 11.95);
+INSERT INTO StoreSells VALUES (NULL, 5, 8, 37.95);
+INSERT INTO StoreSells VALUES (NULL, 5, 9, 7.95);
+INSERT INTO StoreSells VALUES (NULL, 5, 10, 16.95);
+
+
+
+INSERT INTO PairsWith VALUES (NULL, 1, "Queso Manchego", "http://www.foodsubs.com/Photos/agedmanchego.jpg");
+INSERT INTO PairsWith VALUES (NULL, 2, "Skirt Steak", "http://4.bp.blogspot.com/_H1VLin6O1Hc/S-NnEXVF4QI/AAAAAAAAIwg/1dqvU80LXGY/s1600/skirt+steak3.jpg");
+INSERT INTO PairsWith VALUES (NULL, 3, "Seafood", "http://firstcastlefcu.org/wp-content/uploads/2013/05/SEAFOOD-FESTIVAL.jpg");
+INSERT INTO PairsWith VALUES (NULL, 4, "Salad", "http://spoonerdelivery.com/wp-content/uploads/salad.jpg");
+INSERT INTO PairsWith VALUES (NULL, 5, "Thai food", "http://thethaichili.net/site/wp-content/uploads/2013/05/pad-thai.jpg");
+INSERT INTO PairsWith VALUES (NULL, 6, "Oysters", "http://upload.wikimedia.org/wikipedia/commons/8/80/Pacific_oysters.jpg");
+INSERT INTO PairsWith VALUES (NULL, 7, "Seafood", "http://firstcastlefcu.org/wp-content/uploads/2013/05/SEAFOOD-FESTIVAL.jpg");
+INSERT INTO PairsWith VALUES (NULL, 8, "Cookies", "http://www.iwu.edu/wellness/specialevents/images/cookies.jpeg");
+INSERT INTO PairsWith VALUES (NULL, 9, "Gruyere", "http://www.cheesetraders.com/assets/images/products/Cheese/zoom/50-Gruyere.jpg");
+INSERT INTO PairsWith VALUES (NULL, 10, "Lamb", "http://img.foodnetwork.com/FOOD/2007/04/03/Easter_07_lamb_lg.jpg");
+
+
+
+INSERT INTO Reviews VALUES (NULL, 1, "Loved this spanish red, so yummy!");
+INSERT INTO Reviews VALUES (NULL, 2, "Definitely pair with the steak as suggested.");
+INSERT INTO Reviews VALUES (NULL, 3, "Always thought Portugal was only known for its ports, but this dry red is great!");
+INSERT INTO Reviews VALUES (NULL, 4, "Such a cute pink rose- recommend for girls");
+INSERT INTO Reviews VALUES (NULL, 5, "Germans make two thing well - cars and reisling");
+INSERT INTO Reviews VALUES (NULL, 6, "Modest white from Napa valley, an easy option to have on hand.");
+INSERT INTO Reviews VALUES (NULL, 7, "Perfect for a seaside meal");
+INSERT INTO Reviews VALUES (NULL, 8, "This one is worth every penny");
+INSERT INTO Reviews VALUES (NULL, 9, "Tastes like a summer breeze.");
+INSERT INTO Reviews VALUES (NULL, 10, "First time with Chilean wine, and I have to say I'll be back!");
